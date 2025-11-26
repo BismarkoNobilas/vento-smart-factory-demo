3e493508ee999defc28eb8543f5e940bba0d9bfb
bisma <bismarko139@gmail.com>
2025-11-26 15:15:19 +0700

    Header

--- src/components/custom/Clock.jsx
+++ src/components/custom/Clock.jsx
@@ -1,21 +1,27 @@
-// components/Clock.jsx
 "use client";
 import { useApp } from "@/context/AppContext";
 
 export default function Clock() {
   const { now } = useApp();
+
+  const time = now.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+  const weekday = now.toLocaleDateString("en-US", {
+    weekday: "short",
+  }); // Wed
+
+  const dayMonth = now.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+  }); // 27/11
+
   return (
-    <div className="gap-[2px]">
-      {now.toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      })}
-      {now.toLocaleDateString("en-US", {
-        weekday: "short",
-        day: "2-digit",
-        month: "2-digit",
-      })}
+    <div className="whitespace-nowrap font-mono">
+      {time} | {weekday}, {dayMonth}
     </div>
   );
 }
