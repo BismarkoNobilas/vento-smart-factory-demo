a367072dbac0c1fbfc6dd5fa0080491b0fad5167
bisma <bismarko139@gmail.com>
2025-12-10 13:58:54 +0700

    add mqtt test

--- src/lib/getClient.js
+++ src/lib/getClient.js
@@ -7,8 +7,9 @@ let client = null;
 export function getClient() {
   if (!client) {
     const url = process.env.MQTT_URL || "ws://192.168.0.123:8083/mqtt";
-    client = mqtt.connect(url);
-
+    client = mqtt.connect(url, {
+      reconnectPeriod: 2000, // auto reconnect every 2s
+    });
     client.on("connect", () => {
       console.log("✅ MQTT connected:", url);
       client.subscribe("/UploadTopic", (err) => {
