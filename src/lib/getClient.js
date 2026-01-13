f2980daee6927171493d2fb01eabe0c2617d8188
Bismarko Nobilas <bismarko139@gmail.com>
2026-01-13 14:09:00 +0700

    update

--- src/lib/getClient.js
+++ src/lib/getClient.js
@@ -5,13 +5,13 @@ let client = null;
 
 export function getClient() {
   if (!client) {
-    const url = process.env.MQTT_URL || "mqtt://192.168.0.123:1883";
+    const url = process.env.MQTT_URL || "mqtt://192.168.0.120:1883";
     client = mqtt.connect(url);
 
     client.on("connect", () => {
       console.log("✅ MQTT connected:", url);
-      client.subscribe("/UploadTopic", (err) => {
-        if (!err) console.log("📡 Subscribed to /UploadTopic");
+      client.subscribe("/PLCMaketSV", (err) => {
+        if (!err) console.log("📡 Subscribed to /PLCMaketSV");
       });
     });
 
